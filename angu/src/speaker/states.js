fe0126a804079d9f8e16da589b54fe4b5ada1be3
peter@peterkellner.net <peter@peterkellner.net>
2015-11-22 12:47:23 -0800

    cleanup comments

--- angu/src/speaker/states.js
+++ angu/src/speaker/states.js
@@ -5,7 +5,6 @@ var exports = module.exports = function ($stateProvider) {
         url: '/speaker',
         //url: '/',
         templateUrl: '/templates/speaker/speaker.html',
-        //template: '<b>SPEAKER stat.js',
         controller: 'SpeakerController',
         controllerAs: 'speaker'
     });
